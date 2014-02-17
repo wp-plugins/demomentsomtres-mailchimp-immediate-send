0ad8df6cda70f1b3461b1a685775632872e1580b
marcqueralt <marcqueralt@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-17 21:20:15 +0000

    1.1 archive - full catalan translation
    git-svn-id: https://plugins.svn.wordpress.org/demomentsomtres-mailchimp-immediate-send/tags/1.1@859669 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin.php
@@ -0,0 +1,222 @@
+<?php
+/*
+ * Settings and administration
+ * @since 1.0
+ */
+
+add_action('admin_menu', 'dmst_mc_immediate_add_page');
+add_action('admin_init', 'dmst_mc_immediate_admin_init');
+
+/**
+ * Add admin page
+ * @since 1.0 
+ */
+function dmst_mc_immediate_add_page() {
+    add_options_page(__('Mailchimp Immediate', DMST_MC_IMMEDIATE_TEXT_DOMAIN), __('Mailchimp Immediate Send', DMST_MC_IMMEDIATE_TEXT_DOMAIN), 'manage_options', 'dmst_mc_immediate', 'dmst_mc_immediate_option_page');
+}
+
+/**
+ * Admin page contents
+ * @since 1.0
+ */
+function dmst_mc_immediate_option_page() {
+    ?>
+    <div class="wrap" style="float:left;width:50%;">
+        <?php screen_icon(); ?>
+        <h2><?php _e('DeMomentSomTres - MailChimp Immediate Send', DMST_MC_IMMEDIATE_TEXT_DOMAIN); ?></h2>
+        <form action="options.php" method="post">
+            <?php settings_fields('dmst_mc_immediate_options'); ?>
+            <?php do_settings_sections('dmst_mc_immediate'); ?>
+            <br/>
+            <input name="Submit" class="button button-primary" type="submit" value="<?php _e('Save Changes', DMST_MC_IMMEDIATE_TEXT_DOMAIN); ?>"/>
+        </form>
+    </div>
+    <?php
+    echo '<div style="background-color:#eee; width:45%;float:right;padding:10px;">';
+    echo '<h3>' . __('Options', DMST_MC_IMMEDIATE_TEXT_DOMAIN) . '</h3>' . '<pre style="font-size:0.8em;">';
+    print_r(get_option(DMST_MC_IMMEDIATE_OPTIONS));
+    echo '</pre>';
+    echo '</div>';
+}
+
+/**
+ * Admin page options
+ * @since 1.0
+ */
+function dmst_mc_immediate_admin_init() {
+    if (!dmst_mc_immediate_check_requirements())
+        return null;
+
+    register_setting('dmst_mc_immediate_options', 'dmst_mc_immediate_options', 'dmst_mc_immediate_validate_options');
+
+    add_settings_section('dmst_mc_immediate_general', __('General Options', DMST_MC_IMMEDIATE_TEXT_DOMAIN), 'dmst_mc_immediate_section_general', 'dmst_mc_immediate');
+    add_settings_section('dmst_mc_immediate_posttype_options', __('Post Types', DMST_MC_IMMEDIATE_TEXT_DOMAIN), 'dmst_mc_immediate_section_posttypes', 'dmst_mc_immediate');
+
+    add_settings_field('dmst_mc_immediate_apikey', __('Mailchimp API Key', DMST_MC_IMMEDIATE_TEXT_DOMAIN), 'dmst_mc_immediate_field_apikey', 'dmst_mc_immediate', 'dmst_mc_immediate_general');
+
+    dmst_mc_immediate_option_post_types();
+}
+
+/**
+ * Validate admin options
+ * @param array $input the options values as are entered into the forms
+ * @return array the validate options
+ * @since 1.0
+ */
+function dmst_mc_immediate_validate_options($input) {
+    $lists = $input['lists'];
+    $templates = $input['templates'];
+    $newLists = array();
+    foreach ($lists as $posttype => $taxonomies):
+        $newPostTypes = array();
+        foreach ($taxonomies as $taxonomy => $terms):
+            $newTerms = array();
+            foreach ($terms as $term => $content):
+                if ('' != $content):
+                    $newTerms[$term] = $content;
+                endif;
+            endforeach;
+            if (count($newTerms) > 0):
+                $newPostTypes[$taxonomy] = $newTerms;
+            endif;
+        endforeach;
+        if (count($newPostTypes) > 0):
+            $newLists[$posttype] = $newPostTypes;
+        endif;
+    endforeach;
+    $newTemplates = array();
+    foreach ($templates as $posttype => $taxonomies):
+        $newPostTypes = array();
+        foreach ($taxonomies as $taxonomy => $terms):
+            $newTerms = array();
+            foreach ($terms as $term => $content):
+                if ('' != $content):
+                    $newTerms[$term] = $content;
+                endif;
+            endforeach;
+            if (count($newTerms) > 0):
+                $newPostTypes[$taxonomy] = $newTerms;
+            endif;
+        endforeach;
+        if (count($newPostTypes) > 0):
+            $newTemplates[$posttype] = $newPostTypes;
+        endif;
+    endforeach;
+    $input['lists'] = $newLists;
+    $input['templates'] = $newTemplates;
+    $input = array_map('dmst_admin_helper_esc_attr', $input);
+    return $input;
+}
+
+/**
+ * @since 1.0
+ */
+function dmst_mc_immediate_section_general() {
+    
+}
+
+/**
+ * @since 1.0
+ */
+function dmst_mc_immediate_section_posttypes() {
+    echo '<p>' . __('This section shows only post types having any taxonomy with informed values.', DMST_MC_IMMEDIATE_TEXT_DOMAIN) . '</p>';
+    echo '<p>' . __('If the post type is not selected, NOTHING will be sent after publishing content of that post type.', DMST_MC_IMMEDIATE_TEXT_DOMAIN) . '</p>';
+}
+
+/**
+ * @since 1.0
+ */
+function dmst_mc_immediate_section_content() {
+    
+}
+
+/**
+ * @since 1.0
+ */
+function dmst_mc_immediate_field_apikey() {
+    $name = 'API';
+    $value = dmst_admin_helper_get_option(DMST_MC_IMMEDIATE_OPTIONS, $name);
+    dmst_admin_helper_input(DMST_MC_IMMEDIATE_OPTIONS, $name, $value);
+}
+
+/**
+ * @since 1.0
+ */
+function dmst_mc_immediate_option_post_types() {
+    $lists = dmst_mc_immediate_lists();
+    $post_types = dmst_mc_immediate_get_posttypes();
+    foreach ($post_types as $p):
+        //$name = 'post-type-' . $p;
+        $taxonomies = dmst_mc_immediate_get_posttype_taxonomies($p);
+        if (count($taxonomies) > 0):
+            add_settings_field('dmst_mc_immediate_' . $p, $p, 'dmst_mc_immediate_field_posttype', 'dmst_mc_immediate', 'dmst_mc_immediate_posttype_options', array('posttype' => $p));
+            foreach ($taxonomies as $t):
+                $tname = $t;
+                $terms = get_terms(array($tname), array('hide_empty' => false));
+                if (count($terms) > 0):
+                    add_settings_section('dmst_mc_immediate_' . $p . '_' . $tname, $p . ' ' . __('taxonomy', DMST_MC_IMMEDIATE_TEXT_DOMAIN) . ': ' . $tname, 'dmst_mc_immediate_section_content', 'dmst_mc_immediate');
+                    foreach ($terms as $term):
+                        add_settings_field('dmst_mc_immediate_' . $p . '_' . $tname . '_' . $term->slug, __('List for', DMST_MC_IMMEDIATE_TEXT_DOMAIN) . ' ' . $term->slug, 'dmst_mc_immediate_field_mclist', 'dmst_mc_immediate', 'dmst_mc_immediate_' . $p . '_' . $tname, array(
+                            'posttype' => $p,
+                            'taxonomy' => $tname,
+                            'term' => $term->slug
+                                )
+                        );
+                        add_settings_field('dmst_mc_immediate_' . $p . '_' . $tname . '_' . $term->slug . '_template', __('Template for', DMST_MC_IMMEDIATE_TEXT_DOMAIN) . ' ' . $term->slug, 'dmst_mc_immediate_field_mctemplate', 'dmst_mc_immediate', 'dmst_mc_immediate_' . $p . '_' . $tname, array(
+                            'posttype' => $p,
+                            'taxonomy' => $tname,
+                            'term' => $term->slug
+                                )
+                        );
+                    endforeach;
+                endif;
+            endforeach;
+        endif;
+    endforeach;
+}
+
+/**
+ * @since 1.0
+ * @param array $args arguments 'posttype' sets the posttype to show
+ */
+function dmst_mc_immediate_field_posttype($args) {
+    $name = $args['posttype'];
+    $array = dmst_admin_helper_get_option(DMST_MC_IMMEDIATE_OPTIONS, 'posttypes', array());
+    $value = (isset($array[$name])) ? $array[$name] : 'off';
+    dmst_admin_helper_input(DMST_MC_IMMEDIATE_OPTIONS . '[posttypes]', $name, $value, 'checkbox', 'on');
+}
+
+/**
+ * @since 1.0
+ * @param array $args posttype, taxonomy and term must be set
+ */
+function dmst_mc_immediate_field_mclist($args) {
+    $posttype = $args['posttype'];
+    $taxonomy = $args['taxonomy'];
+    $term = $args['term'];
+    $lists = dmst_mc_immediate_lists();
+    $options = dmst_admin_helper_get_option(DMST_MC_IMMEDIATE_OPTIONS, 'lists', array());
+    $options = (isset($options[$posttype])) ? $options[$posttype] : array();
+    $options = (isset($options[$taxonomy])) ? $options[$taxonomy] : array();
+    $value = (isset($options[$term])) ? $options[$term] : null;
+    $prefix = DMST_MC_IMMEDIATE_OPTIONS . "[lists][$posttype][$taxonomy]";
+    dmst_admin_helper_input($prefix, $term, $value, 'list', null, null, '', '', $lists, __('--- Do not send ---', DMST_MC_IMMEDIATE_TEXT_DOMAIN));
+}
+
+/**
+ * @since 1.0
+ * @param array $args posttype, taxonomy and term must be set
+ */
+function dmst_mc_immediate_field_mctemplate($args) {
+    $posttype = $args['posttype'];
+    $taxonomy = $args['taxonomy'];
+    $term = $args['term'];
+    $templates = dmst_mc_immediate_templates();
+    $options = dmst_admin_helper_get_option(DMST_MC_IMMEDIATE_OPTIONS, 'templates', array());
+    $options = (isset($options[$posttype])) ? $options[$posttype] : array();
+    $options = (isset($options[$taxonomy])) ? $options[$taxonomy] : array();
+    $value = (isset($options[$term])) ? $options[$term] : null;
+    $prefix = DMST_MC_IMMEDIATE_OPTIONS . "[templates][$posttype][$taxonomy]";
+    dmst_admin_helper_input($prefix, $term, $value, 'list', null, null, '', '', $templates, __('--- Default ---', DMST_MC_IMMEDIATE_TEXT_DOMAIN));
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,96 @@
+=== DeMomentSomTres MailChimp Immediate Send ===
+Contributors: marcqueralt
+Tags: mailchimp, email, newsletter, notification
+Requires at least: 3.7
+Tested up to: 3.8.1
+Stable tag: head
+
+== Description ==
+
+The DeMomentSomTres Mailchimp Immediate Send plugin allows you to send an automatic message to all the subscribers of some list on content publication.
+
+This plugin is not an alternative to Mandrill the MailChimp platform for transactional email.
+
+= Features =
+
+* Selection based on post type (post, page, custom post)
+* Selection based on categories, post tags and any other taxonomy terms
+* Template Support.
+
+= History & Raison d’être =
+
+While working for Consorci Administració Oberta de Catalunya we integrated Mailchimp and WordPress to perform RSS Campaigns.
+
+Having them on operation the customer faced the need of sending immediate messages when a content was published on a specific category. However, Mailchimp RSS Campaings doesn't allow this because they are launched time based.
+
+So we decide tu build this component that creates an adhoc campaign "regular campaing" every time a content in certain taxonomies is published.
+
+== Installation ==
+
+This portfolio plugin can be installed as any other WordPress plugin. 
+
+= Requirements =
+
+* This plugin uses the MailChimp api by Drew McLellan that requires Curl. Your must have it installed in your server. The plugin itself checks if the curl extension is installed.
+
+== Frequently Asked Questions ==
+
+= I've updated a content and I want to send it again =
+
+You have to check the Force Resend in the top right area called 'Send'.
+
+= The post type I want to use does not appear on the admin page =
+
+Check if this post type:
+
+* has any taxonomy.
+* the taxonomy has values set (with or without elements).
+
+= Why pages are not shown in the admin page =
+
+In default WordPress install classes won't be displayed because pages don't have any taxonomy.
+
+= The content does not appear if I use a template =
+
+This plugin uses the 'std_content00' section (mc:edit) in the MailChimp template.
+
+= Where do I configure the mail contents? =
+
+You can setup a template to make the mails look as you want.
+
+Some of the parameters from the mail are taken from the MailChimp List defaults:
+
+* Subject
+* From name
+* From email
+
+= Where is my message stored in MailChimp? =
+
+The message is stored as a campaign named as the list where it is sent with a YYYY/MM/DD HH:MM:SS suffix.
+
+== Screenshots ==
+
+TBD
+
+== Changelog ==
+
+= 1.1 = 
+* Metabox added to force resend after publishing.
+* Bug Fix: some posttypes sharing taxonomies where not shown.
+
+= 1.0.3 =
+* catalan translation
+
+= 1.0.2 =
+* remove internal git references
+
+= 1.0.1 = 
+* Template support error solved
+
+= 1.0 =
+* Initial version translation ready
+
+= Next Steps =
+
+* Translate it
+* freely choose an edit area for every template
